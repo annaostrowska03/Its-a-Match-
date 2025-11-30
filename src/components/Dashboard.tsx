198601acf796b932f3d791bbc68b3d19a6b0227f
annaostrowska03 <anostrowska11@wp.pl>
2025-11-30 23:20:32 +0100

    Fix missing Button import

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { User } from '../App';
+import { Button } from './ui/button';
 import { RouteInput } from './RouteInput';
 import { UserPreferences } from './UserPreferences';
 import { ReportIssue } from './ReportIssue';
--- src/components/PremiumDashboard.tsx
+++ src/components/PremiumDashboard.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { User } from '../App';
+import { Button } from './ui/button';
 import { PremiumRouteInput } from './PremiumRouteInput';
 import { UserPreferences } from './UserPreferences';
 import { ReportIssue } from './ReportIssue';
